9e0b5a279ae0e5ca779c0ce924cecc40113c75c3
Maria Margarida <125753950+Mmargaridaa@users.noreply.github.com>
2025-10-03 21:15:06 -0300

    Add files via upload

---
+++ VituralBiblioteca/src/main/java/br/edu/unichristus/VituralBiblioteca/VituralBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unichristus.VituralBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VituralBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VituralBibliotecaApplication.class, args);
+	}
+
+}
